3618328065c980f72b406b191ed33dec3d8809cb
Albany Siswanto <albanysiswantoo@gmail.com>
2023-12-11 17:19:43 +0700

    test go modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/albanysiswanto/app-sayHello
+
+go 1.21.1
+
+require github.com/albanysiswanto/go-sayHello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_sayHello "github.com/albanysiswanto/go-sayHello/v2"
+)
+
+func main() {
+	fmt.Println(go_sayHello.SayHello("Bany"))
+}
